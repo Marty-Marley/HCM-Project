9894828a1e515571e5c34550679a708231f872ff
Marty Marley <martin.marley2014@gmail.com>
2019-04-01 00:34:35 +0100

    prod

--- Frontend/common/utils/withApollo.js
+++ Frontend/common/utils/withApollo.js
@@ -1,6 +1,6 @@
 import withApollo from 'next-with-apollo'
 import ApolloClient from 'apollo-boost'
-import { yogaEndpoint } from '../../config'
+import { yogaEndpoint, prodYogaEndpoint } from '../../config'
 
 /**
  * HOC for wrapping apollo for state management
@@ -8,7 +8,7 @@ import { yogaEndpoint } from '../../config'
 export default withApollo(({ headers }) => (
   new ApolloClient({
   // TODO Will need to change the uri in production
-    uri: yogaEndpoint,
+    uri: process.env.NODE_ENV === 'development' ? yogaEndpoint : prodYogaEndpoint,
     request: (operation) => {
       operation.setContext({
         fetchOptions: {
--- Frontend/config.js
+++ Frontend/config.js
@@ -1,2 +1,3 @@
 // Backend endpoint
 export const yogaEndpoint = 'http://localhost:4000'
+export const prodYogaEndpoint = 'https://hm-yoga-prod.herokuapp.com/'
