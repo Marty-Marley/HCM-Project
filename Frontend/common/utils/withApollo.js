a17c4d7155a8eb02fc09a184b2ae63f4480acec3
Marty Marley <martin.marley2014@gmail.com>
2019-04-06 20:56:19 +0100

    Removing last changes +adding log

--- Frontend/common/utils/withApollo.js
+++ Frontend/common/utils/withApollo.js
@@ -1,30 +1,21 @@
 import withApollo from 'next-with-apollo'
-// import ApolloClient from 'apollo-boost'
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import ApolloClient from 'apollo-boost'
 import { yogaEndpoint, prodYogaEndpoint } from '../../config'
 
 /**
  * HOC for wrapping apollo for state management
  */
-export default withApollo(({ headers }) => (
+export default withApollo(({ headers }) => console.log(headers) || (
   new ApolloClient({
-    ssrMode: true,
-    // uri: process.env.NODE_ENV === 'development' ? yogaEndpoint : prodYogaEndpoint,
-    // request: (operation) => {
-    //   operation.setContext({
-    //     fetchOptions: {
-    //       credentials: 'include'
-    //     },
-    //     headers
-    //   })
-    // }
-    link: createHttpLink({
-      uri: process.env.NODE_ENV === 'development' ? yogaEndpoint : prodYogaEndpoint,
-      credentials: 'include',
-      headers
-    }),
-    cache: new InMemoryCache()
+  // TODO Will need to change the uri in production
+    uri: process.env.NODE_ENV === 'development' ? yogaEndpoint : prodYogaEndpoint,
+    request: (operation) => {
+      operation.setContext({
+        fetchOptions: {
+          credentials: 'include'
+        },
+        headers
+      })
+    }
   })
 ))
