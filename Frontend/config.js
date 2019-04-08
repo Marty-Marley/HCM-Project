c210898a55c331ace77da4f18b7e813c990c7d56
Marty Marley <martin.marley2014@gmail.com>
2019-04-08 21:29:39 +0100

    Pointing to EU backend

--- Frontend/config.js
+++ Frontend/config.js
@@ -1,3 +1,3 @@
 // Backend endpoint
 export const yogaEndpoint = 'http://localhost:4000'
-export const prodYogaEndpoint = 'https://hm-yoga-prod.herokuapp.com/'
+export const prodYogaEndpoint = 'https://hu-man-backend.herokuapp.com/'
