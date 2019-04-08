5509c8969bf7d5ad67bbaf2aa0848d61d8c599fe
Marty Marley <martin.marley2014@gmail.com>
2019-04-08 22:55:21 +0100

    removing compression from custom server

--- Frontend/server.js
+++ Frontend/server.js
@@ -6,7 +6,6 @@ const { createServer } = require('http')
 const { parse } = require('url')
 const { createReadStream } = require('fs')
 const next = require('next')
-const compression = require('compression')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -14,7 +13,6 @@ const handle = app.getRequestHandler()
 const port = process.env.PORT || 3000
 
 app.prepare().then(() => {
-  app.use(compression())
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true)
     const { pathname } = parsedUrl
