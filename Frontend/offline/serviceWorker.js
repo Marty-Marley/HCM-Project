ddf3d17f42f89d40765baeaff5f2756092b85cd3
Marty Marley <martin.marley2014@gmail.com>
2019-05-07 13:40:37 +0100

    Reverting to simple SW

--- Frontend/offline/serviceWorker.js
+++ Frontend/offline/serviceWorker.js
@@ -1,68 +1,66 @@
 const CACHE_NAME = 'simple-cache-v1';
 const urlsToCache = ['/login', '/', '/my_profile', '/record_time', '/my_team', '/permissions'];
 
-// self.addEventListener('install', (event) => {
-//   const preLoaded = caches.open(CACHE_NAME)
-//     .then(cache => cache.addAll(urlsToCache))
-//   event.waitUntil(preLoaded);
-// });
-
-// self.addEventListener('fetch', (event) => {
-//   const response = caches.match(event.request)
-//     .then(match => match || fetch(event.request));
-//   event.respondWith(response);
-// });
-
-// Tried credentials: include - no diff
-
 self.addEventListener('install', (event) => {
-  event.waitUntil(preLoad());
+  const preLoaded = caches.open(CACHE_NAME)
+    .then(cache => cache.addAll(urlsToCache))
+  event.waitUntil(preLoaded);
 });
-
-var preLoad = function () {
-  console.log('Installing web app');
-  return caches.open('offline').then((cache) => {
-    console.log("caching index and important routes");
-    return cache.addAll(urlsToCache);
-  });
-};
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(checkResponse(event.request).catch(function() {
-    return returnFromCache(event.request);
-  }));
-  event.waitUntil(addToCache(event.request));
+  const response = caches.match(event.request)
+    .then(match => match || fetch(event.request));
+  event.respondWith(response);
 });
 
-var checkResponse = function (request) {
-  return new Promise(((fulfill, reject) => {
-    fetch(request).then(function(response){
-      if(response.status !== 404) {
-        fulfill(response);
-      } else {
-        reject();
-      }
-    }, reject);
-  }));
-};
+// self.addEventListener('install', (event) => {
+//   event.waitUntil(preLoad());
+// });
 
-var addToCache = function (request) {
-  return caches.open('offline').then((cache) => {
-    return fetch(request).then(function (response) {
-      console.log(response.url + " was cached");
-      return cache.put(request, response);
-    });
-  });
-};
+// var preLoad = function () {
+//   console.log('Installing web app');
+//   return caches.open('offline').then((cache) => {
+//     console.log("caching index and important routes");
+//     return cache.addAll(urlsToCache);
+//   });
+// };
 
-var returnFromCache = function (request) {
-  return caches.open('offline').then((cache) => {
-    return cache.match(request).then(function (matching) {
-     if(!matching || matching.status == 404) {
-       return cache.match("offline.html");
-     } else {
-       return matching;
-     }
-    });
-  });
-};
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(checkResponse(event.request).catch(function() {
+//     return returnFromCache(event.request);
+//   }));
+//   event.waitUntil(addToCache(event.request));
+// });
+
+// var checkResponse = function (request) {
+//   return new Promise(((fulfill, reject) => {
+//     fetch(request).then(function(response){
+//       if(response.status !== 404) {
+//         fulfill(response);
+//       } else {
+//         reject();
+//       }
+//     }, reject);
+//   }));
+// };
+
+// var addToCache = function (request) {
+//   return caches.open('offline').then((cache) => {
+//     return fetch(request).then(function (response) {
+//       console.log(response.url + " was cached");
+//       return cache.put(request, response);
+//     });
+//   });
+// };
+
+// var returnFromCache = function (request) {
+//   return caches.open('offline').then((cache) => {
+//     return cache.match(request).then(function (matching) {
+//      if(!matching || matching.status == 404) {
+//        return cache.match("offline.html");
+//      } else {
+//        return matching;
+//      }
+//     });
+//   });
+// };
